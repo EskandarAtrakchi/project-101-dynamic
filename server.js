fd1c726215262e1f41133f669ed46659dc9ce766
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2024-04-06 14:10:00 +0100

    code updated

--- server.js
+++ server.js
@@ -1,39 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const bip39 = require('bip39');
-const axios = require('axios');
 const app = express();
-
 const port = process.env.PORT || 3010;
 
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
+
     const response = 'working fine!';
     console.log(response); 
-    res.send(response); // Send response back to the client
+    
 });
-
 
 app.get('/wallet-generate.html', (req, res) => {
     // Generate the seed phrase
     const seedPhrase = bip39.generateMnemonic();
     // Send the seed phrase as JSON response
     res.json({ seedPhrase });
-});
-
-app.get('/charts.html', async (req, res) => {
-    try {
-        //wait for axios to get the API response 
-        const response = await axios.get('https://api.alternative.me/v1/ticker/');
-        const responseData = response.data;
-
-        res.json(responseData);
-    } catch (error) {
-        console.error('Error fetching data from external API:', error.message);
-        res.status(500).json({ error: 'Error fetching data from external API' });
-    }
 });
 
 app.listen(port, () => {
