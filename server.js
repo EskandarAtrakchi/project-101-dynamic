59d781ea63ba4f3ffbef5cc44bc0556bd581f1bf
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2024-04-06 14:03:48 +0100

    axios required backed updated

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bip39 = require('bip39');
+const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3010;
 
