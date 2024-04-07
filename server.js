ff52b554abfe90ab750debfe42130972922874a2
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2024-04-07 19:50:46 +0100

    code updated

--- server.js
+++ server.js
@@ -1,26 +1,46 @@
+const axios = require('axios');
 const express = require('express');
 const cors = require('cors');
 const bip39 = require('bip39');
 const app = express();
+
 const port = process.env.PORT || 3010;
 
 app.use(express.json());
 app.use(cors());
-
+ 
+//route number one index 
 app.get('/', (req, res) => {
-
-    const response = 'working fine!';
-    console.log(response); 
-    
+    //just to see if the server is running
+    res.json({ message: 'tested! working.' });
+    console.log(res.json());
 });
 
 app.get('/wallet-generate.html', (req, res) => {
+
     // Generate the seed phrase
     const seedPhrase = bip39.generateMnemonic();
     // Send the seed phrase as JSON response
     res.json({ seedPhrase });
+
+});
+
+// route number two ticker 
+app.get('/charts.html', async (req, res) => {
+    try {
+        //wait for axios to get the API response 
+        const response = await axios.get('https://api.alternative.me/v1/ticker/');
+        const responseData = response.data;
+
+        res.json(responseData);
+    } catch (error) {
+        console.error('Error fetching data from external API:', error.message);
+        res.status(500).json({ error: 'Error fetching data from external API' });
+    }
 });
 
 app.listen(port, () => {
+
     console.log(`Server is running on http://localhost:${port}`);
+
 });
