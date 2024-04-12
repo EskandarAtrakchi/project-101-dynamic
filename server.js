fff599e3a27b3e11b133c36914b4e3135a0ff306
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2024-04-12 22:14:51 +0100

    code updated

--- server.js
+++ server.js
@@ -2,26 +2,22 @@ const axios = require('axios');
 const express = require('express');
 const cors = require('cors');
 const bip39 = require('bip39');
+const fetch = require('node-fetch');
 const app = express();
 
 const port = process.env.PORT || 3010;
 
 app.use(express.json());
 app.use(cors());
- 
-//route number one index 
+
 app.get('/', (req, res) => {
-    //just to see if the server is running
     res.json({ message: 'tested! working.' });
     console.log(res.json());
 });
 
 app.get('/wallet-generate.html', (req, res) => {
-
     try {
-        // Generate the seed phrase
         const seedPhrase = bip39.generateMnemonic();
-        // Send the seed phrase as JSON response
         res.json({ seedPhrase });
     } catch (error) {
         console.error('Error fetching data from backend:', error.message);
@@ -29,13 +25,10 @@ app.get('/wallet-generate.html', (req, res) => {
     }
 });
 
-// route number two ticker 
 app.get('/charts.html', async (req, res) => {
     try {
-        //wait for axios to get the API response 
         const response = await axios.get('https://api.alternative.me/v1/ticker/');
         const responseData = response.data;
-
         res.json(responseData);
     } catch (error) {
         console.error('Error fetching data from external API:', error.message);
@@ -43,8 +36,20 @@ app.get('/charts.html', async (req, res) => {
     }
 });
 
+app.get('/fetchAssets/:address', async (req, res) => {
+    const address = req.params.address;
+    const response = await fetch(`https://deep-index.moralis.io/api/v2.2/wallets/${address}/tokens?chain=eth&exclude_spam=true&exclude_unverified_contracts=true`, 
+    {
+        method: 'GET', 
+        headers: {
+            'Content-Type': 'application/json',
+            'X-API-Key': process.env.REACT_APP_MORALIS_API_KEY
+        },
+    });
+    const data = await response.json();
+    res.send(data);
+});
+
 app.listen(port, () => {
-
-    console.log(`Server is running on http://localhost:${port}`);
-
+    console.log(`Server running at http://localhost:${port}`);
 });
